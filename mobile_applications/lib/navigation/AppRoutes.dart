2f851d8b5dd87a960db8b1f0701a8e65919dc7ce
Yassin Hassan <sim.YassinHassan81522@alexu.edu.eg>
2026-04-29 15:43:21 +0300

    remove mobile task project

--- mobile_applications/lib/navigation/AppRoutes.dart
+++
@@ -1,8 +0,0 @@
-class AppRoutes {
-  static const String homeScreen      = '/';
-  static const String favoritesScreen = '/favorites';
-  static const String detailScreen    = '/detail';
-  static const String signupScreen    = '/signup';
-  static const String loginScreen     = '/login';
-}
-
